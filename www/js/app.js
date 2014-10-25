9511f066e5433e0ed47cde6c1b5af7fb78b792be
Will Roscoe <wroscoe@gmail.com>
2014-10-25 14:22:14 -0700

    not much

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('openapp', ['ionic'])
+angular.module('openapp', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -11,13 +11,17 @@ angular.module('openapp', ['ionic'])
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      window.bluetoothle = cordova.plugins.BluetoothLePlugin;
+      console.log('cordova present');
+      console.log(window.bluetoothle);
+      console.log(cordova.plugins);
+      console.log(window);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
-
 
 
 .controller('OpenCtrl', function($scope) {
@@ -27,4 +31,32 @@ angular.module('openapp', ['ionic'])
     { title: 'Get high enough to grab the flag' },
     { title: 'Find the Princess' }
   ];
+
+  $scope.scanBLE = function() {
+    console.log('scanBLE');
+    console.log(navigator);
+    
+    var params = {};
+
+    var err = function (e) {
+      console.log(e);
+    };
+
+    window.bluetoothle.initialize(function() {
+      console.log('intializing bluetooth');
+      window.bluetoothle.startScan(function(status) {
+        console.log('scan successful');
+        console.log(status);
+      },
+
+
+
+      err, params);}
+
+    , err, params);
+
+
+
+    alert("scanBLUE");
+  }
 });
